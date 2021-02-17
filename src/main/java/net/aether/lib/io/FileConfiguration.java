645658b57cc9f1c208858766862f5676ced0d563
Cheos <cheoscrafter@gmail.com>
2021-02-17 19:16:34 +0100

    update javadocs, remove javadoc plugin
    the javadoc plugin doesn't like me, removed it for now
    also updated some javadocs
    
    Signed-off-by: Cheos <cheoscrafter@gmail.com>

--- src/main/java/net/aether/lib/io/FileConfiguration.java
+++ src/main/java/net/aether/lib/io/FileConfiguration.java
@@ -14,16 +14,16 @@ public interface FileConfiguration {
 
 	// Load \\
 	/**
-	 * (re)loads the configuration from the supplied File  
-	 * @param file
-	 * @throws IOException
+	 * (re)loads the configuration from the supplied File
+	 * @param file the file
+	 * @throws IOException if an error occurs
 	 */
 	public void load(File file) throws IOException;
 	/**
 	 * Loads the configuration using the supplied String.<br>
 	 * This could search for a file with the string's path, or allow for parsing values returned by {@link #asString()}.<br>
 	 * Please look into the javadoc of the implementation.
-	 * @param string
+	 * @param string a path as a string
 	 */
 	public void load(String string);
 	
@@ -32,23 +32,23 @@ public interface FileConfiguration {
 	 * Saves the configuration to the supplied file.<br>
 	 * Convenience method: calls save without prettyPrint.<br>
 	 * <b>NOTE:</b> prettyPrint might not be supported, or make any difference.
-	 * @param file
-	 * @throws IOException
+	 * @param file the file to save to
+	 * @throws IOException if an error occurs
 	 */
-	public default void save(File file) 			 throws IOException { save(file, false); };
+	public default void save(File file) throws IOException { save(file, false); };
 	/**
 	 * Saves the configuration to the supplied file.<br>
-	 * If implemented/supported, will save the configuration in a more easily readable Format when prettyPrint = true  
-	 * @param file
-	 * @param prettyPrint
-	 * @throws IOException
+	 * If implemented/supported, will save the configuration in a more easily readable Format when prettyPrint = true
+	 * @param file the file to save to
+	 * @param prettyPrint if the output should be formatted nice
+	 * @throws IOException if an error occurs
 	 */
 	public void save(File file, boolean prettyPrint) throws IOException;
 	/**
 	 * Returns a String, containing all information of the configuration.<br>
-	 * It is recommended, that {@link #toString()} would return the exact same value, but this might not always be the case.<br>
+	 * It is recommended, that {@link Object#toString() toString()} would return the exact same value, but this might not always be the case.<br>
 	 * Please look into the javadoc of the implementation
-	 * @return
+	 * @return a string representation of this file
 	 */
 	public String asString();
 	
@@ -59,21 +59,21 @@ public interface FileConfiguration {
 	 * This could either just check the filename, location or even the contents.<br>
 	 * Please look into the javadoc of the implementation<br><br>
 	 * If not implemented, will default to true
-	 * @param fiel
+	 * @param file the file to check
 	 * @return true if the configuration supports the file, false otherwise.
 	 */
-	public default boolean isFileSupported(File fiel) { return true; }
+	public default boolean isFileSupported(File file) { return true; }
 	
 	// Fallback Value \\
 	/**
 	 * Sets the fallback value of <span style="color:orange">path</span>, which will be used if the value is either not set or invalid.
-	 * @param path
-	 * @param newDefault
+	 * @param path the path to set a fallback value for
+	 * @param fallback the fallback value
 	 */
 	public void setFallback(String path, String fallback);
 	/**
 	 * Gets the fallback value of <span style="color:orange">path</span>.<br>
-	 * @param path
+	 * @param path the path to get the fallback value for
 	 * @return the fallback value as a string, or null if no fallback value has been set
 	 */
 	public String getFallback(String path);
@@ -82,27 +82,27 @@ public interface FileConfiguration {
 	/**
 	 * Sets the type of <span style="color:orange">path</span>.<br>
 	 * If the FileConfiguration has a shorter String associated with the class, it would be used instead.
-	 * @param path
-	 * @param type
+	 * @param path the path
+	 * @param type the type
 	 */
 	public void setType(String path, Class<?> type);
 	/**
 	 * Sets the type of <span style="color:orange">path</span> to the supplied string.<br>
 	 * This type can only be parsed correctly, if there is a class associated with this string.
-	 * @param path
-	 * @param type
+	 * @param path the path
+	 * @param type tha type
 	 */
 	public void setType(String path, String type);
 	
 	/**
 	 * Gets the type of <span style="color:orange">path</span>.
-	 * @param path
+	 * @param path the path
 	 * @return the name of the type, or null if no type was specified
 	 */
 	public String getTypeName(String path);
 	/**
 	 * Gets the type of <span style="color:orange">path</span>.
-	 * @param path
+	 * @param path the path
 	 * @return the type as a class
 	 * @throws ClassNotFoundException if the stored type string is not associated with any class.
 	 */
@@ -112,8 +112,8 @@ public interface FileConfiguration {
 	/**
 	 * Sets the value of <span style="color:orange">path</span>.<br>
 	 * If the value is of wrong type, will leave the path as it was before.
-	 * @param path
-	 * @param value
+	 * @param path the path
+	 * @param value the value
 	 */
 	public void set(String path, Object value);
 	/**
@@ -121,30 +121,30 @@ public interface FileConfiguration {
 	 * If the value is of a wrong type, tries to parse it into the correct one.<br>
 	 * If the value could not be parsed, tries to use the fallback value.<br>
 	 * If there is no fallback value, will leave the path as it was before.
-	 * @param path
-	 * @param value
+	 * @param path the path
+	 * @param value the value
 	 */
 	public void trySet(String path, Object value);
 	/**
 	 * Sets the value of <span style="color:orange">path</span>.<br>
 	 * If the value is of a wrong type, the type will be updated to the values type.
-	 * @param path
-	 * @param value
+	 * @param path the path
+	 * @param value the value
 	 */
 	public void forceSet(String path, Object value);
 	
 	// Comments \\
 	/**
 	 * Returns the comment of <span style="color:orange">path</span>.
-	 * @param path
+	 * @param path the path
 	 * @return The comment, or null if the path does not exist
 	 */
 	public String[] getComment(String path);
 	/**
 	 * Sets the comment of <span style="color:orange">path</span>.<br>
 	 * If the comment is empty it will be removed completely.
-	 * @param path
-	 * @param comment
+	 * @param path the path
+	 * @param comment the comment
 	 */
 	public void setComment(String path, String... comment);
 	
@@ -175,14 +175,15 @@ public interface FileConfiguration {
 	 * <li>double</li>
 	 * <li>String</li>
 	 * </ul>
-	 * @param path
+	 * @param path the path
 	 * @return The parsed object, or null if the value does not exist
 	 */
 	public Object get(String path);
 	/**
 	 * Gets the value of <span style="color:orange">path</span> as the supplied class.<br>
-	 * @param path
-	 * @param parseToClass
+	 * @param path the path
+	 * @param parseToClass the type to cast to
+	 * @param <T> a type
 	 * @return the parsed object
 	 * @throws InvalidTypeException if the value could not be parsed to the supplied class.
 	 */
@@ -191,41 +192,41 @@ public interface FileConfiguration {
 	// Standard parsed getter \\
 	/**
 	 * Gets the value of <span style="color:orange">path</span> as a boolean.<br>
-	 * @param path
+	 * @param path the path
 	 * @return the parsed boolean
 	 * @throws InvalidTypeException if the value could not be parsed as boolean.
 	 */
 	public boolean 	getBoolean(String path) throws InvalidTypeException;
 	/**
 	 * Gets the value of <span style="color:orange">path</span> as an integer.<br>
-	 * @param path
+	 * @param path the path
 	 * @return the parsed integer
 	 * @throws InvalidTypeException if the value could not be parsed as integer.
 	 */
 	public int 		getInt	  (String path) throws InvalidTypeException;
 	/**
 	 * Gets the value of <span style="color:orange">path</span> as a String.<br>
-	 * @param path
+	 * @param path the path
 	 * @return the parsed string
 	 */
 	public String 	getString (String path);
 	/**
 	 * Gets the value of <span style="color:orange">path</span> as a float.<br>
-	 * @param path
+	 * @param path the path
 	 * @return the parsed float
 	 * @throws InvalidTypeException if the value could not be parsed as float.
 	 */
 	public float 	getFloat  (String path) throws InvalidTypeException;
 	/**
 	 * Gets the value of <span style="color:orange">path</span> as a double.<br>
-	 * @param path
+	 * @param path the path
 	 * @return the parsed double
 	 * @throws InvalidTypeException if the value could not be parsed as double.
 	 */
 	public double	getDouble (String path) throws InvalidTypeException;
 	/**
 	 * Gets the value of <span style="color:orange">path</span> as a long.<br>
-	 * @param path
+	 * @param path the path
 	 * @return the parsed long
 	 * @throws InvalidTypeException if the value could not be parsed as long.
 	 */
